535873c03ee066a3c303efcb3c07ed31eb2e4a2f
Gabriel Goto <gabrielcosta08@gmail.com>
2024-06-16 17:09:25 +0100

    Remove unused test

--- tests/build_logs_tests.sh
+++ tests/build_logs_tests.sh
@@ -21,12 +21,4 @@ testLogsUrl() {
     assertEquals "log url did not match" "$expected_url" "$actual_url"
 }
 
-testNotStreamingForOnHoldBuild() {
-    local expected_message="Build on-hold"
-    local not_expected_message="jq: error (at <stdin>:1): Cannot iterate over null (null)"
-    local actual_message=$(process_build "on-hold_status_response.json")
-    assertContains "Message did not contain the expected content:" "$actual_message" "$expected_message"
-    assertNotContains "Logs contained the unexpected content:" "$actual_message" "$not_expected_message" 
-}
-
 . ./tests/shunit2
